f93ffe03bb2a6fe82d90c67416f1a6bfac3daa94
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-23 21:45:30 +0300

    copy api teas

---
+++ RealEstate/Resource/RealEstate-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RealEstate-Bridging-Header.h
+//  RealEstate
+//
+//  Created by Andrey Chernyshev on 23.03.2021.
+//
+
+#ifndef RealEstate_Bridging_Header_h
+#define RealEstate_Bridging_Header_h
+
+#import <RushSDK/RushSDK.h>
+
+#endif /* RealEstate_Bridging_Header_h */
