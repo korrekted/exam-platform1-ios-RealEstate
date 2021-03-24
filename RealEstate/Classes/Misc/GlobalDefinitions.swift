f9f0edaf3f98adcd1b9b0513d0e15ad6bb8cee9f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-24 22:31:17 +0300

    new bundle id

--- RealEstate/Classes/Misc/GlobalDefinitions.swift
+++ RealEstate/Classes/Misc/GlobalDefinitions.swift
@@ -17,7 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
     
-    static let appStoreId = "1559138664"
+    static let appStoreId = "1559966609"
     
     static let termsOfServiceUrl = "https://docs.google.com/document/d/1-W9vqg-sno9DcYcDo-XhT26TMhYE1mOU6OivK2IhI7M/edit"
     static let privacyPolicyUrl = "https://docs.google.com/document/d/1GCIelmdgxFi7_dLgg0O_RzfHZB3PMYlczk9gekCRlIE/edit"
