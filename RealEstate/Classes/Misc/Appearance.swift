d326b357a4739dd9997f0ceb1019653f6da60a42
akonst17 <akonst17@gmail.com>
2022-04-13 11:48:41 +0300

    upgrade; build 15

--- RealEstate/Classes/Misc/Appearance.swift
+++ RealEstate/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 255, green: 115, blue: 99)
     static let progress2Color = UIColor(integralRed: 255, green: 155, blue: 38)
     static let progress3Color = UIColor(integralRed: 255, green: 196, blue: 45)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- RealEstate/Classes/Service/Stats/Share/StatsShareManager.swift
+++ RealEstate/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.realestate.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.realestate.widget")
     }
     
     @discardableResult
