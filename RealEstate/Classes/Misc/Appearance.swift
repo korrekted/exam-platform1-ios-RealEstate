ce095668075527f7eadaef13ee1c1ae03492ef54
akonst17 <akonst17@gmail.com>
2022-08-01 11:29:44 +0300

    обновления Nursing; build 16

--- RealEstate/Classes/Misc/Appearance.swift
+++ RealEstate/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 252, green: 250, blue: 242)
-    
     static let mainColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let mainColorAlpha = UIColor(integralRed: 255, green: 115, blue: 99, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 255, green: 155, blue: 38)
-    
-    static let progress1Color = UIColor(integralRed: 255, green: 115, blue: 99)
-    static let progress2Color = UIColor(integralRed: 255, green: 155, blue: 38)
-    static let progress3Color = UIColor(integralRed: 255, green: 196, blue: 45)
-    
+    static let secondColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let thirdColor = UIColor(integralRed: 255, green: 209, blue: 48)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 252, green: 250, blue: 242)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
